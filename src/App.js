4a62e01ae91762228fec04216c69a2b5d4537209
Timineri Okoko <tonyeokoko@outlook.com>
2024-06-06 23:22:12 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,53 @@
-import { useState } from "react";
+// import { useState } from "react";
 
+// const messages = [
+//   "Learn React ⚛️",
+//   "Apply for jobs 💼",
+//   "Invest your new income 🤑",
+// ];
+
+// export default function App() {
+//   const [step, setStep] = useState(1);
+
+//   function handlePrevious() {
+//     if (step > 1) setStep(step - 1);
+//   }
+
+//   function handleNext() {
+//     if (step < 3) setStep(step + 1);
+//   }
+
+//   return (
+//     <div className="steps">
+//       <div className="numbers">
+//         <div className={step >= 1 ? "active" : ""}>1</div>
+//         <div className={step >= 2 ? "active" : ""}>2</div>
+//         <div className={step >= 3 ? "active" : ""}>3</div>
+//       </div>
+
+//       <p className="message">
+//         Step {step}: {messages[step - 1]}
+//       </p>
+
+//       <div className="buttons">
+//         <button
+//           onClick={handlePrevious}
+//           style={{ backgroundColor: "#7950f2", color: "#FFF" }}
+//         >
+//           Previous
+//         </button>
+//         <button
+//           onClick={handleNext}
+//           style={{ backgroundColor: "#7950f2", color: "#FFF" }}
+//         >
+//           Next
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+import React from "react";
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -7,39 +55,23 @@ const messages = [
 ];
 
 export default function App() {
-  const [step, setStep] = useState(1);
-
-  function handlePrevious() {
-    if (step > 1) setStep(step - 1);
-  }
-
-  function handleNext() {
-    if (step < 3) setStep(step + 1);
-  }
-
+  const step = 1;
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
+        <div className={`${step === 1 ? "active" : ""}`}>1</div>
+        <div className={`${step === 2 ? "active" : ""}`}>2</div>
+        <div className={`${step === 3 ? "active" : ""}`}>3</div>
       </div>
-
       <p className="message">
-        Step {step}: {messages[step - 1]}
+        Step: {step} {messages[step - 1]}
       </p>
 
       <div className="buttons">
-        <button
-          onClick={handlePrevious}
-          style={{ backgroundColor: "#7950f2", color: "#FFF" }}
-        >
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
           Previous
         </button>
-        <button
-          onClick={handleNext}
-          style={{ backgroundColor: "#7950f2", color: "#FFF" }}
-        >
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
           Next
         </button>
       </div>
